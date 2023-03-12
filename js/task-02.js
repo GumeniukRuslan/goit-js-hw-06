0ed375a213cc0f7b5f6994de514fb4c09557eb4a
Gumeniuk Ruslan <bishob1997@gmail.com>
2023-03-12 11:30:08 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients')
+
+const appendIngredients = ingredients => {
+  const append = []
+
+  ingredients.forEach(el => {
+    const ingredient = document.createElement('li');
+    ingredient.textContent = el;
+    ingredient.classList.add('item')
+    append.push(ingredient)
+    
+  })
+
+  return ingredientsList.append(...append)
+}
+
+appendIngredients(ingredients);
