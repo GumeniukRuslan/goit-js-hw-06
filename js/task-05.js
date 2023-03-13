37444b79025f19efd191b199231c8d89f5c62a11
Gumeniuk Ruslan <bishob1997@gmail.com>
2023-03-13 10:07:07 +0200

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const inputValue = document.querySelector('#name-input')
+const outputValue = document.querySelector('#name-output')
+
+
+inputValue.addEventListener('input', event => {
+  outputValue.textContent = event.currentTarget.value
+  if (!event.currentTarget.value) {
+    outputValue.textContent = 'Anonymous'
+  }
+})
