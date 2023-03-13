fcc25fa9746e42d5f24053a61d692dfeeff8c0dc
Gumeniuk Ruslan <bishob1997@gmail.com>
2023-03-13 20:21:03 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector('.login-form')
+const emailInput = document.querySelector('[name="email"]')
+const passInput = document.querySelector('[name="password"]')
+const submitBtn = document.querySelector('[type="submit"]')
 
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  if (!emailInput.value || !passInput.value) {
+    return alert('Всі поля повинні бути заповнені!')
+  }
+  const userData = {};
+  userData.email = emailInput.value;
+  userData.password = passInput.value;
+  console.log(userData);
+  formEl.reset();
+})
