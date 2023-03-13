fbb4d672799441986d9071e0523fb4fdbfa2e158
Gumeniuk Ruslan <bishob1997@gmail.com>
2023-03-13 20:30:51 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', event => {
+
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = bodyEl.style.backgroundColor;
+})
