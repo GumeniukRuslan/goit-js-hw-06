fd777eb17a9bf5ef04a9cd005008cd9e95baabff
Gumeniuk Ruslan <bishob1997@gmail.com>
2023-03-14 09:15:23 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector('#controls');
+const inputValue = controls.querySelector('input'); 
+const createBtn = controls.querySelector('[data-create]'); 
+const destroyBtn = controls.querySelector('[data-destroy]');
+const resultBox = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  if (inputValue.value > 100 || inputValue.value < 1) {
+    return alert('Введіть число від 1 до 100!');
+  }
+
+  const boxesToAppend = []
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = 30 + (i * 10) + 'px';
+    box.style.height = 30 + (i * 10) + 'px';
+    boxesToAppend.push(box);
+  }
+  return resultBox.append(...boxesToAppend);
+}
+
+createBtn.addEventListener('click', () => createBoxes(inputValue.value));
+destroyBtn.addEventListener('click', () => {
+  resultBox.innerHTML = '';
+  inputValue.value = '';
+});
